d83815bcb825ff604e60062c2779f13ecf2625c9
Mohammad Ahmad <49246417+shahim92@users.noreply.github.com>
2019-11-30 17:08:52 -0500

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,20 @@
+package Fifth_Code;
+
+public class Object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Referance_variable build = new Referance_variable();
+		build.number(100, 200, 300);
+		Object test= new Object();
+		test.task();
+		
+		
+
+	}
+	
+	public void task() {
+		System.out.println("This is a non- static method: ");
+	}
+
+}
