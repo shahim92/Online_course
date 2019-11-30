e60be1731357586404a9ccd3d268bba01b1d4b31
Mohammad Ahmad <49246417+shahim92@users.noreply.github.com>
2019-11-30 17:08:06 -0500

    Add files via upload

---
+++ sixth_Code/A.java
@@ -0,0 +1,14 @@
+package sixth_Code;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B obj = new B();
+		obj.shahim();
+		obj.addition();
+		obj.subtract(200, 100);
+
+	}
+
+}
---
+++ sixth_Code/B.java
@@ -0,0 +1,18 @@
+package sixth_Code;
+
+public class B {
+	public void shahim() {
+		System.out.println("THis is shahim");
+	}
+	public void addition() {
+		int a = 500;
+		int b= 300;
+		int c = a+b;
+		System.out.println(c);
+	}
+	public void subtract(int r, int n) {
+		int m = r-n;
+		System.out.println(m);
+	}
+
+}
